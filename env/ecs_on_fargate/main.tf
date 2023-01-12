9a14ef08cf25071556ad8db827a44df93a805dfd
naruse666 <become15@i.softbank.jp>
2023-01-12 22:08:49 +0900

    add: ecs on fargate

---
+++ env/ecs_on_fargate/main.tf
@@ -0,0 +1,46 @@
+module "ecs" {
+  source = "../../modules/ecs"
+  name   = "ecs-on-fargate"
+  kms_key_id = aws_kms_key.ecs.arn
+  log_group = aws_cloudwatch_log_group.ecs.name
+}
+
+module "ecs-sg" {
+  source = "../../modules/sg"
+  name = "test-ecs-sg"
+  vpc_id = module.vpc.vpc_id
+  port = 80
+  cidr = ["0.0.0.0/0"]
+}
+
+resource "aws_ecs_service" "test" {
+  name = "test-task"
+  cluster = module.ecs.id
+  task_definition = aws_ecs_task_definition.test.arn
+  desired_count = 1
+  launch_type = "FARGATE"
+  platform_version   = "1.4.0"
+  network_configuration {
+    assign_public_ip = true
+    subnets = module.vpc.public_subnets
+    security_groups = [module.ecs-sg.id]
+  }
+}
+
+resource "aws_ecs_task_definition" "test" {
+  family = "test-task"
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu = 256
+  memory = 512
+  container_definitions = file("./task.json")
+}
+
+resource "aws_kms_key" "ecs" {
+  description             = "ecs encription"
+  deletion_window_in_days = 7
+}
+
+resource "aws_cloudwatch_log_group" "ecs" {
+  name = "ecs-log"
+}
---
+++ env/ecs_on_fargate/vpc.tf
@@ -0,0 +1,13 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "ecs-on-fargate-vpc"
+  azs  = ["ap-northeast-1a", "ap-northeast-1d"]
+  cidr = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  #デフォルトセキュリティグループのルール削除
+  manage_default_security_group  = true
+  default_security_group_ingress = []
+  default_security_group_egress  = []
+}
