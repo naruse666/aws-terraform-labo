6702c0d20f09f3078013e10a4f873747cc8b1569
naruse666 <become15@i.softbank.jp>
2023-01-14 21:51:58 +0900

    add: s3 cloudfront static website

---
+++ env/s3_cloudfront_static_website/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket = "naruse-tf-backend"
+    key    = "aws-labo/s3_cloudfront_static_website.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
---
+++ env/s3_cloudfront_static_website/s3.tf
@@ -0,0 +1,55 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket_prefix = "static-website"
+}
+
+resource "aws_s3_bucket_website_configuration" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_policy" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.static-website.json
+}
+
+data "aws_iam_policy_document" "static-website" {
+  statement {
+    sid    = "Allow CloudFront"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.static-website.iam_arn]
+    }
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.bucket.arn}/*"
+    ]
+  }
+}
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "index.html"
+  source = "index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "error.html"
+  source = "error.html"
+  content_type = "text/html"
+}
