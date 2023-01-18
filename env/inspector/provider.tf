f6fe53e5324977c06b1bb952dda5e030d32eab7c
naruse666 <become15@i.softbank.jp>
2023-01-18 21:13:17 +0900

    add: inspector

---
+++ env/inspector/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket = "naruse-tf-backend"
+    key    = "aws-labo/inspector.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
